1db5476e84af08da132ab5a7d5a9956aebfba571
sozykinsa <sergey.sozykin@gmail.com>
2022-02-01 22:42:15 +0500

    typos in helpers

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -15,11 +15,13 @@ def spacedel(stroka):
             stroka = stroka.replace('  ', ' ')
         return stroka
 
+
 def float_to_string(fl):
         res = '{0:12.8f}'.format(fl)
         if res == "-0.00000000":
             res = " 0.00000000"
         return res
+
 
 def is_number(row):
         try:
@@ -27,6 +29,7 @@ def is_number(row):
             return True
         except ValueError:
             return False
+
 
 def is_integer(n):
         try:
@@ -36,19 +39,24 @@ def is_integer(n):
         else:
             return float(n).is_integer()
 
+
 def list_plus_list(x, y):
         """ поэлементное сложение списков """
         return list(map(lambda a, b: a + b, x, y))
+
 
 def float_mult_list(x, y):
         """ умножение числа на список """
         return [float(x * item) for item in y]
 
+
 def list_str_to_float(x):
         return [float(item) for item in x]
 
+
 def list_str_to_int(x):
         return [int(item) for item in x]
+
 
 def nearest(latEn, nextLat):
         """ This example shows how to """
@@ -57,6 +65,7 @@ def nearest(latEn, nextLat):
             if res > abs(float(latEn[i][0]) - nextLat):
                 res = abs(float(latEn[i][0]) - nextLat)
         return res
+
 
 def NextLat(latEn, eps):
         """ This example shows how to """
@@ -87,8 +96,7 @@ def NextLat(latEn, eps):
             next_lat = -b / (2 * a)
             sign = 1
             st = 0
-            # errxrange = AT.Helpers.errorsrange(latEn)
-            while (nearest(latEn, nextLat) < eps / 4) and (st < 5):
+            while (nearest(latEn, next_lat) < eps / 4) and (st < 5):
                 if (len(latEn) <= imin + sign) or (imin + sign < 0):
                     next_lat = float(latEn[imin][0]) + sign * eps / 2
                 else:
@@ -96,14 +104,16 @@ def NextLat(latEn, eps):
                 sign = -sign
                 st = st + 1
             if st == 5:
-                nextLat = 0
-        return nextLat
+                next_lat = 0
+        return next_lat
+
 
 def write_text_to_file(fname, text):
         f = open(fname, 'w')
         print(text, file=f)
         f.close()
-        
+
+
 def getsubs(dir):
         dirs = []
         subdirs = []
@@ -133,32 +143,34 @@ def cdev(ii, jj):
                 i = j
                 j = ir
 
-def mini(List2D):
-        """ Сортирует список по возрастанию первого столбца и возвращает индекс минимального элемента во втором столбце """
-        List2D = sorted(List2D, key=itemgetter(0))
-        imin = 0
-        for i in range(1, len(list_2d)):
-            if float(list_2d[i][1]) < float(list_2d[imin][1]):
-                imin = i
-        return imin
 
-def ListN2Split(DATA):
-        # из списка N x 2 получаем 2 списка по N элементов
-        x = []
-        y = []
-        for row in data:
-            x.append(row[0])
-            y.append(row[1])
-        return np.array(x), np.array(y)
-    
-def errorsrange(ListLatEn):
+def mini(list_2d):
+    """ Сортирует список по возрастанию первого столбца и возвращает индекс минимального элемента во втором столбце """
+    list_2d = sorted(list_2d, key=itemgetter(0))
+    imin = 0
+    for i in range(1, len(list_2d)):
+        if float(list_2d[i][1]) < float(list_2d[imin][1]):
+            imin = i
+    return imin
+
+
+def ListN2Split(data):
+    x = []
+    y = []
+    for row in data:
+        x.append(row[0])
+        y.append(row[1])
+    return np.array(x), np.array(y)
+
+
+def errorsrange(list_lat_en):
         """ Возвращает ширину доверительного интервала при поиске оптимального параметра """
         list_lat_en = sorted(list_lat_en, key=itemgetter(0))
         if len(list_lat_en) < 3:
             return 10
 
-        ans = float(ListLatEn[len(ListLatEn)-1][0]) - float(ListLatEn[0][0])
-        imin = mini(ListLatEn)
+        ans = float(list_lat_en[-1][0]) - float(list_lat_en[0][0])
+        imin = mini(list_lat_en)
         if imin == 0:
             ans = float(list_lat_en[1][0]) - float(list_lat_en[0][0])
         if imin == len(list_lat_en)-1:
@@ -193,7 +205,7 @@ def property_from_sub_file(filename, k, prop, count, typen):
                 if (str1 != '') and (str1.find("%include") >= 0):
                     new_f = str1.split()[1]
                     file = os.path.dirname(filename) + "/" + new_f
-                    is_found, k, property = Helpers.property_from_sub_file(file, k, prop, count, typen)
+                    is_found, k, property = property_from_sub_file(file, k, prop, count, typen)
                 if (str1 != '') and (str1.find(prop) >= 0):
                     str1 = str1.replace(prop, ' ')
                     if typen == "unformatted":
@@ -219,17 +231,18 @@ def property_from_sub_file(filename, k, prop, count, typen):
             MyFile.close()
         return is_found, k, property
 
+
 def RoundToPlane(atom, R):
-        """ RoundToPlane  """
+    """ RoundToPlane  """
+    z = atom.z
+    fi = math.asin(atom.x/R)
+    if atom.y <= -1e-3:
+        fi = 3.14 - fi
+    x = -R*fi
+    return [x, z]
 
-        z = atom.z
-        fi = math.asin(atom.x/R)
-        if atom.y <= -1e-3:
-            fi = 3.14 - fi
-        x = -R*fi
-        return [x, z]
 
-def dos_from_file(filename, n, nlines = 0):
+def dos_from_file(filename, n, nlines=0):
         DOSFile = open(filename)
         strDOS = DOSFile.readline()
         energy = []
@@ -249,6 +262,7 @@ def dos_from_file(filename, n, nlines = 0):
                 strDOS = read_row_of_dos_file(DOSFile, energy, n, spinDown, spinUp, strDOS)
         return energy, spinDown, spinUp
 
+
 def read_row_of_dos_file(DOSFile, energy, n, spinDown, spinUp, strDOS):
         line = strDOS.split(' ')
         line1 = []
@@ -263,6 +277,7 @@ def read_row_of_dos_file(DOSFile, energy, n, spinDown, spinUp, strDOS):
             spinDown.append(0)
         strDOS = DOSFile.readline()
         return strDOS
+
 
 def utf8_letter(let):
         if let == r'\Gamma':
