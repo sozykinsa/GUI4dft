2e18f9d0b25e137128e9834601fd18f758e4e81d
serge <sozykinsa@susu.ru>
2022-02-03 20:31:46 +0500

    tests: parse charges from output

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -45,15 +45,15 @@ def test_importer_xyz(tests_path):
 
 def test_importer_struct_out(tests_path):
     f_name = str(tests_path / 'ref_data' / 'h2o-ang' / "siesta.STRUCT_OUT")
-    model, fdf = Importer.Import(f_name, fl='all', prop=False, xyzcritic2=False)
+    model, fdf = Importer.Import(f_name, fl='all', prop=True, xyzcritic2=False)
     assert len(model[0].atoms) == 3
 
     f_name = str(tests_path / 'ref_data' / 'h2o-bohr' / "siesta.STRUCT_OUT")
-    model, fdf = Importer.Import(f_name, fl='all', prop=False, xyzcritic2=False)
+    model, fdf = Importer.Import(f_name, fl='all', prop=True, xyzcritic2=False)
     assert len(model[0].atoms) == 3
 
     f_name = str(tests_path / 'ref_data' / 'h2o-zmatrix' / "siesta.STRUCT_OUT")
-    model, fdf = Importer.Import(f_name, fl='all', prop=False, xyzcritic2=False)
+    model, fdf = Importer.Import(f_name, fl='all', prop=True, xyzcritic2=False)
     assert len(model[0].atoms) == 3
 
 
